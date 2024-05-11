dc085ededea606b20da409dc92a337c1166fc952
Equipo1 <Equipo1@DESKTOP-R1O8MG4>
2024-05-11 09:52:22 -0600

    Agregar archivos de proyecto.

---
+++ Migrations/20240503224053_IdentityDBUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace tablaJquery.Migrations
+{
+    public partial class IdentityDBUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    MiddleName = table.Column<string>(nullable: true),
+                    Contact = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
---
+++ Models/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace tablaJquery.Models
+{
+    public class AppDBContext: DbContext 
+    {
+        private readonly DbContextOptions _options;
+
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+        public DbSet<User> users { get; set; }
+    }
+}
